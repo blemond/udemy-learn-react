2dbc16b9b9e13e729ac470641c19c86257023db6
Ben Lemond <ben.lemond@matrixteam.com>
2022-06-17 15:57:19 -0500

    add react-app-5

---
+++ react-app-5/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './containers/App';
+import 'tachyons';
+
+import './index.css';
+
+//ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
